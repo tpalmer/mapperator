f0e844f1fa0e93a33463d8fd0ce87af04c19bb21
Travis Palmer <travis@cramerdev.com>
2012-08-11 20:04:53 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapperator::Application.config.secret_token = '39cdfb9dbee04aac1e1424aaf9809ec130d2017f525c92095f408ecebb2c0d0aac788ea26496fcf44488255d790b56c57be48aab8279479b655c878b4480dacf'
